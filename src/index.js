633bf09cd7522405cbc719ecb0ea46075658589f
yokuronai <rfabini1996@gmail.com>
2020-04-14 16:11:18 -0300

    Voltando para o CORS liberado

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: 'http://201.59.138.54' }));
-//app.use(cors({origin: 'http://localhost:8888'}));
+app.use(cors());
+//app.use(cors({origin: 'http://201.59.138.54'})); // FUNCIONA DE VERDADE!!!
 
 app.use(require('./routes/index'));
 
