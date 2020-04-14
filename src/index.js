b9160ef478e738a07a3e60ed40f0d5809e5a5be3
yokuronai <rfabini1996@gmail.com>
2020-04-14 16:01:00 -0300

    Add CORS para teste de restrição

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: 'http://201.59.138.54' }));
 //app.use(cors({origin: 'http://localhost:8888'}));
 
 app.use(require('./routes/index'));
