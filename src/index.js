fb08d744362fe277dc21a2840508046f7db32992
yokuronai <rfabini1996@gmail.com>
2020-04-14 15:48:37 -0300

    Add CORS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
-
+var cors = require('cors');
 
 const app = express();
 
@@ -8,6 +8,8 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+//app.use(cors({origin: 'http://localhost:8888'}));
 
 app.use(require('./routes/index'));
 
